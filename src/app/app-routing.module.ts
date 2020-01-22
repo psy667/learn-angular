74dcbd142ce7d24665a27944303579b38c464e89
Arseniy <arseniyoguzov@gmail.com>
2020-01-22 19:32:33 +0300

    redirect guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import {AboutComponent} from './about/about.component';
 import {PostsComponent} from './posts/posts.component';
 import {PostComponent} from './post/post.component';
 import {AboutExtraComponent} from './about-extra/about-extra.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
+import {AuthGuard} from './auth.guard';
 
 // http://localhost:4200/ -> HomeComponent
 // http://localhost:4200/about -> AboutComponent
@@ -13,12 +15,14 @@ import {AboutExtraComponent} from './about-extra/about-extra.component';
 const routes: Routes = [
     {path: '', component: HomeComponent},
     {
-        path: 'about', component: AboutComponent, children: [
+        path: 'about', component: AboutComponent, canActivateChild: [AuthGuard], children: [
             {path: 'extra', component: AboutExtraComponent}
         ]
     },
-    {path: 'posts', component: PostsComponent},
-    {path: 'posts/:id', component: PostComponent}
+    {path: 'posts', component: PostsComponent, canActivate: [AuthGuard]},
+    {path: 'posts/:id', component: PostComponent},
+    {path: 'error', component: ErrorPageComponent},
+    {path: '**', redirectTo: '/error'}
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import {Component} from '@angular/core'
+import {Component} from '@angular/core';
+import {AuthService} from './auth.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() {}
+    constructor(private auth: AuthService) {
+
+    }
 }
 
