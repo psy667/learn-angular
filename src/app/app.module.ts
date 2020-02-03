a736f03bdd72f474dfc71977cd096d9fdb0e44f0
psy667 <arseniyoguzov@gmail.com>
2020-02-03 17:44:47 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser'
-import {NgModule} from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms'
-import {AboutComponent} from './about/about.component'
-import {HomeComponent} from './home/home.component'
-import {PostsComponent} from './posts/posts.component'
-import {PostComponent} from './post/post.component'
-import {AboutExtraComponent} from './about-extra/about-extra.component'
-import {AppRoutingModule} from './app-routing.module'
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent,
-    HomeComponent,
-    PostsComponent,
-    PostComponent,
-    AboutExtraComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
