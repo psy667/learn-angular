a0df4854df78c3739d1b9d0550a2f7b55dec93b4
psy667 <arseniyoguzov@gmail.com>
2020-03-23 11:32:18 +0300

    added components

---
+++ src/app/kanban/board-list/board-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {Board} from "../board.model";
+import {Subscription} from "rxjs";
+import {BoardService} from "../board.service";
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
+
+@Component({
+  selector: 'app-board-list',
+  templateUrl: './board-list.component.html',
+  styleUrls: ['./board-list.component.scss']
+})
+export class BoardListComponent implements OnInit {
+  boards: Board[];
+  sub: Subscription;
+
+  constructor(
+    public boardService: BoardService
+  ) { }
+
+  ngOnInit() {
+    this.sub = this.boardService
+      .getUserBoards()
+      .subscribe(boards => (this.boards = boards))
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe()
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.boards, event.previousIndex, event.currentIndex);
+    this.boardService.sortBoards(this.boards)
+  }
+}
